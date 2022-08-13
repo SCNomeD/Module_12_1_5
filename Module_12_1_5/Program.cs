077737b3d9389109220a87f165f99ef4983caf5f
Владимир Червяков <vladiiimir1991@gmail.com>
2022-08-13 14:55:05 +0300

    Добавьте файлы проекта.

---
+++ Module_12_1_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using Module_12_1_5;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        User[] arr = new User[] {
+                new User("Evgen", "Евгений", false),
+                new User("Vertuoz", "Алексей", true),
+                new User("NomeD", "Владимир", false)
+            };
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i].Greeting();
+            if (arr[i].IsPremium != true)
+            {
+                ShowAds();
+            }
+        }
+    }
+
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(2000);
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        Thread.Sleep(3000);
+        Console.WriteLine();
+    }
+}
---
+++ Module_12_1_5/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Module_12_1_5
+{
+    class User
+    {
+        public User(string login, string name, bool isPremium)
+        {
+            Login = login ?? throw new ArgumentNullException(nameof(login));
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            IsPremium = isPremium;
+        }
+
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Hello, {Name}!\n");
+        }
+    }
+}
